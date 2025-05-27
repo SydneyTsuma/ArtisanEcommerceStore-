ab89ffd82a6f878f404b52f7350a23559ecda88b
Sydney Tsuma <sydneytsuma1@gmail.com>
2025-05-27 03:15:33 +0300

    "supabaseclient default"

--- apps/pyEmarketBackend/supabaseclient.py
+++ apps/pyEmarketBackend/supabaseclient.py
@@ -1,7 +1,7 @@
 from supabase import create_client, Client
 
-url = "https://lvpeuzvnmldgvmogqfvm.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2cGV1enZubWxkZ3Ztb2dxZnZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxNDk2ODgsImV4cCI6MjA2MzcyNTY4OH0.Lyv8Ag9XchXXzPOv1cbRMqFIQtY8t7QSh7CQzLZCHZE"
+url = ""
+key = ""
 supabase: Client = create_client(url, key)
 
 # Example: fetch users
